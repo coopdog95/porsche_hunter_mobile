0d6519354715d87c05ccd12ae868687865f860f9
Cooper Timmerman <coopert925@gmail.com>
2022-10-03 15:50:17 -0700

    fetches for create/update

--- requests/hunts.js
+++ requests/hunts.js
@@ -6,3 +6,13 @@ export const getAllHunts = async () => await fetchApi('/hunts?all=true', 'GET')
 
 export const getHuntById = async huntId =>
   await fetchApi(`/hunts/${huntId}`, 'GET')
+
+export const updateHunt = async (huntId, huntProps, cars) => {
+  const body = JSON.stringify({ huntProps, cars })
+  return await fetchApi(`/hunts/${huntId}`, 'PATCH', { body })
+}
+
+export const createHunt = async (huntProps, cars) => {
+  const body = JSON.stringify({ huntProps, cars })
+  return await fetchApi(`/hunts`, 'POST', { body })
+}
