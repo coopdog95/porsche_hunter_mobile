c08ddddee63e31bf90a7c809a31e9fbac1b1d670
Cooper Timmerman <coopert925@gmail.com>
2022-10-03 15:55:31 -0700

    Update carListItem.js

--- components/Hunts/carListItem.js
+++ components/Hunts/carListItem.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, Image } from 'react-native'
 import styles from './styles/carListItem'
+import { models, trimsByModel } from '../../services/vehicleData'
 
-const CarListItem = ({ car: { model, trim, year, image_url: imageUrl } }) => {
-  useEffect(() => {}, [])
-
-  const serializedTrim = ['none', null].includes(trim?.toLowerCase())
-    ? ''
-    : trim
-
-  const carTitle = `${year} Porsche ${model} ${serializedTrim}`
-  const imageSource = {
-    uri: 'https://www.pcarmarket.com/static/media/uploads/galleries/photos/uploads/galleries/2019-porsche-911-targa-4-gts/.thumbnails/IMG_0043.jpg/IMG_0043-tiny-2048x0-0.5x0.jpg',
-    cache: Platform.OS === 'ios' ? 'only-if-cached' : null,
+const CarListItem = ({ car: { model, trim, image_url: imageUrl } }) => {
+  const carTitle = () => {
+    const modelName = models.find(m => m.value === model)?.label
+    const trimName =
+      trimsByModel[modelName]?.find(t => t.value === trim)?.label || 'none'
+    return `Porsche ${modelName}${
+      !!trimName && trimName !== 'none' ? ` ${trimName}` : ''
+    }`
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.carTitle}>{carTitle}</Text>
+      <Text style={styles.carTitle}>{carTitle()}</Text>
       <View style={styles.imageWrapper}>
-        <Image style={styles.image} source={imageSource} resizeMode="contain" />
+        <Image
+          style={styles.image}
+          source={{ uri: imageUrl }}
+          resizeMode="contain"
+        />
       </View>
     </View>
   )
