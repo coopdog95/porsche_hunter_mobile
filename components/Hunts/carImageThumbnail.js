7704e2bddaf0395299a9da3d5be25614d606f840
Cooper Timmerman <coopert925@gmail.com>
2022-10-04 19:55:00 -0700

    implicit return + removed state

--- components/Hunts/carImageThumbnail.js
+++ components/Hunts/carImageThumbnail.js
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Image, View } from 'react-native'
 import styles from './styles/carImageThumbnail'
 
-const CarImageThumbnail = ({ imageUrl }) => {
-  const [loaded, setLoaded] = useState(false)
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {}, [])
-
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: imageUrl }}
-        resizeMode="cover"
-        style={styles.image}
-      />
-    </View>
-  )
-}
+const CarImageThumbnail = ({ imageUrl }) => (
+  <View style={styles.container}>
+    <Image source={{ uri: imageUrl }} resizeMode="cover" style={styles.image} />
+  </View>
+)
 
 export default CarImageThumbnail
