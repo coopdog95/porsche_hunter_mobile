bbdedd13f9063e18cd7f4bd41773c11fea9846a2
Cooper Timmerman <coopert925@gmail.com>
2022-10-04 22:25:26 -0700

    better spinner color

--- components/common/Spinner.js
+++ components/common/Spinner.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
 
-const Spinner = () => <ActivityIndicator size="large" color="#cca447" />
+const red = '#a22b33'
+
+const Spinner = () => <ActivityIndicator size="large" color={red} />
 
 export default Spinner
